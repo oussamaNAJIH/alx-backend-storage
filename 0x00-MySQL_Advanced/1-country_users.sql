207f76cdfb19ce2f7a51c6a571fde79914f45148
oussamaNAJIH <oussamanajih6@gmail.com>
2024-04-25 19:59:11 +0200

    v

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- users table
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
