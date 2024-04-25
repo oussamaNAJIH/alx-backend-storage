fde44fdb81fc736d69ac2348a3e4691069f88f74
oussamaNAJIH <oussamanajih6@gmail.com>
2024-04-25 20:22:20 +0200

    Old school band

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- lists all bands with Glam rock as their main style, ranked by their longevity
+
+SELECT band_name, (split - formed) AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC;
